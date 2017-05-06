b054e1cce19c84e9f0f646723f36d09d2b045a54
alfonso zbook <zbook@reyesaguayo.com>
2017-05-06 08:46:50 -0500

    change Java ProjectileApp a little bit to allow being called from rJava

--- 2015PortableWS/osp_csm/src/org/opensourcephysics/sip/ch03/ProjectileApp.java
+++ 2015PortableWS/osp_csm/src/org/opensourcephysics/sip/ch03/ProjectileApp.java
@@ -71,8 +71,12 @@ public class ProjectileApp extends AbstractSimulation {
    * Starts the Java application.
    * @param args  command line parameters
    */
-  public static void main(String[] args) {
-    SimulationControl.createApp(new ProjectileApp());
+//  public static void main(String[] args) {
+//    SimulationControl.createApp(new ProjectileApp());
+//  }
+  
+  public void startProjectile() {
+	  SimulationControl.createApp(new ProjectileApp());
   }
 }
 
